fd3ba01b240b961a9c32f761d0325c4bad8a0551
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2024-02-17 06:55:35 +0000

    ci: check configuration of CMake for MSVC in CI

---
+++ scripts/ci_win.cmd
@@ -0,0 +1,16 @@
+call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\BuildTools\\VC\\Auxiliary\\Build\\vcvarsall.bat" x86_x64
+
+cmake .. ^
+    -G Ninja ^
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache ^
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache ^
+    -DCMAKE_TOOLCHAIN_FILE=..\vcpkg\scripts\buildsystems\vcpkg.cmake ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DMSGPACK_LIGHT_TESTING:BOOL=ON ^
+    -DMSGPACK_LIGHT_ENABLE_UNIT_TESTS=ON ^
+    -DMSGPACK_LIGHT_ENABLE_INTEG_TESTS=ON ^
+    -DMSGPACK_LIGHT_ENABLE_BENCH=OFF ^
+    -DMSGPACK_LIGHT_TEST_BENCHMARKS=OFF ^
+    -DMSGPACK_LIGHT_BUILD_EXAMPLES=OFF ^
+    -DMSGPACK_LIGHT_WRITE_JUNIT:BOOL=ON ^
+    -DBUILD_SHARED_LIBS:BOOL=ON
