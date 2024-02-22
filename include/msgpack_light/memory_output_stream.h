b54f7a81d89f74387c4c1cc3c2d0673d3843d13d
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2024-02-22 15:13:56 +0000

    refactor: use new binary class in memory_output_stream class

--- include/msgpack_light/memory_output_stream.h
+++ include/msgpack_light/memory_output_stream.h
@@ -31,37 +31,6 @@
 
 namespace msgpack_light {
 
-namespace details {
-
-/*!
- * \brief Calculate the size of an expanded buffer.
- *
- * \param[in] current_size Current size.
- * \param[in] additional_size Additional size.
- * \return Size of the expanded buffer.
- */
-[[nodiscard]] inline std::size_t calculate_expanded_memory_buffer_size(
-    std::size_t current_size, std::size_t additional_size) {
-    std::size_t next_size = current_size;
-    while (true) {
-        next_size *= 2;
-        if (next_size <= current_size) {
-            // Overflow
-            const std::size_t max_size =
-                std::numeric_limits<std::size_t>::max();
-            if (max_size - current_size >= additional_size) {
-                return max_size;
-            }
-            throw std::bad_alloc();
-        }
-        if (next_size - current_size >= additional_size) {
-            return next_size;
-        }
-    }
-}
-
-}  // namespace details
-
 /*!
  * \brief Class of streams to write data to memory.
  */
@@ -70,24 +39,7 @@ public:
     /*!
      * \brief Constructor.
      */
-    memory_output_stream()
-        : buffer_(
-              static_cast<unsigned char*>(std::malloc(initial_buffer_size))),
-          capacity_(initial_buffer_size) {
-        if (buffer_ == nullptr) {
-            throw std::bad_alloc();
-        }
-    }
-
-    memory_output_stream(const memory_output_stream&) = delete;
-    memory_output_stream(memory_output_stream&&) = delete;
-    memory_output_stream& operator=(const memory_output_stream&) = delete;
-    memory_output_stream& operator=(memory_output_stream&&) = delete;
-
-    /*!
-     * \brief Destructor.
-     */
-    ~memory_output_stream() { std::free(buffer_); }
+    memory_output_stream() = default;
 
     /*!
      * \brief Write data.
@@ -96,22 +48,7 @@ public:
      * \param[in] size Size of the data.
      */
     void write(const unsigned char* data, std::size_t size) override {
-        const std::size_t remaining = capacity_ - written_;
-        if (remaining < size) {
-            const std::size_t new_capacity =
-                details::calculate_expanded_memory_buffer_size(
-                    capacity_, size - remaining);
-            auto* new_buffer = static_cast<unsigned char*>(
-                std::realloc(buffer_, new_capacity));
-            if (new_buffer == nullptr) {
-                throw std::bad_alloc();
-            }
-            buffer_ = new_buffer;
-            capacity_ = new_capacity;
-        }
-
-        std::memcpy(buffer_ + written_, data, size);
-        written_ += size;
+        buffer_.append(data, size);
     }
 
     /*!
@@ -119,36 +56,27 @@ public:
      *
      * \return Pointer to the written data.
      */
-    [[nodiscard]] const unsigned char* data() const noexcept { return buffer_; }
+    [[nodiscard]] const unsigned char* data() const noexcept {
+        return buffer_.data();
+    }
 
     /*!
      * \brief Get the size of the written data.
      *
      * \return Size of the written data.
      */
-    [[nodiscard]] std::size_t size() const noexcept { return written_; }
+    [[nodiscard]] std::size_t size() const noexcept { return buffer_.size(); }
 
     /*!
      * \brief Get the data as msgpack_light::binary object.
      *
      * \return binary object.
      */
-    [[nodiscard]] binary as_binary() const { return binary(data(), size()); }
+    [[nodiscard]] const binary& as_binary() const { return buffer_; }
 
 private:
-    //! Size of the initial buffer.
-    static constexpr std::size_t initial_buffer_size = 4096U;
-
-    static_assert(initial_buffer_size > details::static_memory_buffer_size);
-
-    //! Pointer to the current buffer.
-    unsigned char* buffer_;
-
-    //! Size of the allocated buffer.
-    std::size_t capacity_;
-
-    //! Size of the written data.
-    std::size_t written_{0U};
+    //! Buffer.
+    binary buffer_;
 };
 
 }  // namespace msgpack_light
